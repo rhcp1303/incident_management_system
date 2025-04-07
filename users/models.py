bcbd4566e740ea8bddf0f562b76b46c4b5b31d75
ankit anand <ankit.anand@ankits-MacBook-Air.local>
2025-04-07 16:02:15 +0530

    separting css,html and js

--- users/models.py
+++ users/models.py
@@ -17,26 +17,6 @@ class User(AbstractUser):
     mobile_number = models.CharField(max_length=20, blank=False, null=False)
     fax = models.CharField(max_length=20, blank=True, null=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name=('groups'),
-        blank=True,
-        help_text=(
-            'The groups this user belongs to. A user will get all permissions '
-            'granted to each of their groups.'
-        ),
-        related_name="incident_management_users_groups",
-        related_query_name="user",
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name=('user permissions'),
-        blank=True,
-        help_text=('Specific permissions for this user.'),
-        related_name="incident_management_users_permissions",
-        related_query_name="user",
-    )
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'address', 'country', 'state', 'city', 'pincode', 'mobile_isd_code',
                        'mobile_number']
